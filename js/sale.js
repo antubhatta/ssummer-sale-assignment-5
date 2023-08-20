b194a33556851b27c3d1c54b7a6b456526707e95
antubhatta <antubhatta09@gmail.com>
2023-08-20 23:15:13 +0600

    total price added

--- js/sale.js
+++ js/sale.js
@@ -2,9 +2,18 @@ function cardClickBtn(target){
     const card = target.parentNode.querySelector('.card')
     const productName= card.querySelector('.product-name').innerText
 
-    // const productPrice= card.querySelector('.product-price').textContent
     const li= document.createElement('li')
     li.innerText=productName
     const selectedProduct=document.getElementById('selected-product')
     selectedProduct.appendChild(li)
+
+    const productPrice= card.querySelector('.product-price').textContent.split(' ')[0]
+    const itemPrice=parseFloat(productPrice)
+    
+    const totalPrice= document.getElementById('total-price')
+    const totalPriceString=totalPrice.innerText
+    const previousTotalPrice=parseFloat(totalPriceString)
+    
+    const currentTotalPrice= itemPrice+previousTotalPrice
+    totalPrice.innerText=currentTotalPrice.toFixed(2)
 }
